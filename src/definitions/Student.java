743f4de0a1ee07774aea37c7814dbf349fde30ef
Avanee Dheer <avanee.dheer_cs19@gla.ac.in>
2020-08-30 22:09:49 +0530

    add non-parameterized constructor to Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -14,6 +14,7 @@ public class Student {
     private long universityRollNumberOfTheStudent;
     private int numberOfBooksIssuedByTheStudent;
     private Book[] namesOfTheBooksIssuedByTheStudent;
+    private static final int MAXIMUM_BOOKS_STUDENT_CAN_ISSUE = 5;
 
     // Parameterized Constructor
     public Student(String nameOfTheStudent, long universityRollNumberOfTheStudent, int numberOfBooksIssuedByTheStudent, Book[] namesOfTheBooksIssuedByTheStudent) {
@@ -21,5 +22,16 @@ public class Student {
         this.universityRollNumberOfTheStudent = universityRollNumberOfTheStudent;
         this.numberOfBooksIssuedByTheStudent = numberOfBooksIssuedByTheStudent;
         this.namesOfTheBooksIssuedByTheStudent = namesOfTheBooksIssuedByTheStudent;
+    }
+
+    // Non-Parameterized Constructor
+    public Student() {
+        this.nameOfTheStudent = "";
+        this.universityRollNumberOfTheStudent = 0L;
+        this.numberOfBooksIssuedByTheStudent = 0;
+        this.namesOfTheBooksIssuedByTheStudent = new Book[MAXIMUM_BOOKS_STUDENT_CAN_ISSUE];
+        for (int initializingBook = 0; initializingBook < MAXIMUM_BOOKS_STUDENT_CAN_ISSUE; initializingBook++) {
+            this.namesOfTheBooksIssuedByTheStudent[initializingBook] = new Book();
+        }
     }
 }
